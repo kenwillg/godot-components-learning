f40c7a51d6b8dcefeb14765c4b8077142d9a1884
Kenneth <kenwillg85@gmail.com>
2025-08-05 01:27:34 +0700

    contracts done

--- src/fish.gd
+++
@@ -1,7 +0,0 @@
-extends CharacterBody2D
-
-@export var SPEED:= -200
-
-func _physics_process(delta: float) -> void:
-	velocity = Vector2(SPEED, 0)
-	move_and_slide()
--- src/projectile/projectile.gd
+++ src/projectile/projectile.gd
@@ -1,9 +1,12 @@
 extends Sprite2D
 
 @export var SPEED:= 400
+@export var damage:= 25
 
 func _physics_process(delta: float) -> void:
 	global_position += Vector2(SPEED * delta, 0)
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("hit!")
+	if body.has_method("take_damage"):
+		body.take_damage(damage)
+		queue_free()
